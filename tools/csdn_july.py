d9431b2338081e66334d8604dde2c3cfbc727f7c
shengbin_cao <shengbin.c@everstring.com>
2015-04-20 20:16:21 +0800

    add csdn july article parser.

---
+++ tools/csdn_july.py
@@ -0,0 +1,38 @@
+import StringIO
+import datetime
+import re
+import time
+import urllib2
+
+from BeautifulSoup import BeautifulSoup, Comment, Tag
+
+from utils import soup_wrapper
+from utils.soup_wrapper import getPageContent
+
+
+def get_all_pages():
+    lists = []
+    for i in range(1, 6) :
+        lists.append("http://blog.csdn.net/v_JULY_v/article/list/%d" %i)
+    return lists   
+
+def get_page_articles(page_link):
+    req_header={'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    'Accept':'text/html;q=0.9,*/*;q=0.8',
+    'Accept-Charset':'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    'Connection':'close',
+    'Referer':None
+    }
+    page = BeautifulSoup(soup_wrapper.getPageContent(page_link, req_header))
+    article_spans = page.findAll(attrs={"class" : "link_title"})
+    for article_span in article_spans :
+        article_link = "http://blog.csdn.net%s" %article_span.a["href"]
+        print article_link
+        article = BeautifulSoup(soup_wrapper.getPageContent(article_link, req_header), convertEntities=BeautifulSoup.HTML_ENTITIES)
+        content = article.find(attrs={"id" : "article_content"})
+        print content.get_text()
+        break
+
+if __name__ == '__main__':
+   lists = get_all_pages()
+   get_page_articles(lists[0])
